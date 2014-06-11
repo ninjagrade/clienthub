bcb2235cf82f619229a6baa770bc69b66b62af5c
Phil <ninjagrade@gmail.com>
2014-06-11 21:59:41 +0100

    Early commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clienthub::Application.config.secret_token = '83a7eba5c0f519eaf5a565b70ee30a733cda494430c09738ce2435dee272501d1070ae7886ffc0f757839e854d62a60ae1cddaaa4a239b3d2374c0a22b00b17e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Clienthub::Application.config.session_store :cookie_store, key: '_clienthub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Clienthub::Application.config.session_store :active_record_store
